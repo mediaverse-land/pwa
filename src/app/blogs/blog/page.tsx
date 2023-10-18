916a25f77691c0420a2a9011a5014dfd60bbdb04
Darkness <alimurtaza7x@gmail.com>
2023-10-18 17:33:54 +0330

    single-blog

--- src/app/blogs/blog/page.tsx
+++ src/app/blogs/blog/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const Blog = () => {
-    return (<div className="w-full flex justify-center mt-36 space-x-10">
+    return (<div className="w-full flex justify-center flex-col items-center sm:items-start sm:flex-row mt-36 space-x-0 px-4  sm:space-x-10">
         <div className="flex flex-col">
             <div className="w-80 h-80 bg-cover bg-[url('/images/image-room.png')]">
             </div>
@@ -11,9 +11,9 @@ const Blog = () => {
             </button>
         </div>
         <div>
-            <h1 className="text-white text-4xl">My thoughts</h1>
+            <h1 className="text-white text-4xl mt-8 sm:mt-0">My thoughts</h1>
             <p className="text-blue-500 mt-2">About Basic Display API</p>
-            <p className="text-white w-[650px]">The Instagram Basic Display API allows users of your app to get basic profile information, photos, and videos in their Instagram accounts.
+            <p className="text-white w-full sm:w-[650px]">The Instagram Basic Display API allows users of your app to get basic profile information, photos, and videos in their Instagram accounts.
                 The API can be used to access any type of Instagram account but only provides read-access to basic data. If you are building an app that will allow Instagram Businesses or Creators to publish media, moderate comments, identify @mentioned and hashtagged media, or get data about other Instagram users, use the Instagram Graph API instead.
             </p>
             <p className="text-white text-xl mt-8">Common Uses</p>
@@ -23,7 +23,7 @@ const Blog = () => {
                 <li>Get an Instagram user’s images, videos, and albums</li>
             </ul>
             <p className="text-white text-xl mt-8">Limitations</p>
-            <ul className="text-white list-disc text-xs w-[650px]">
+            <ul className="text-white list-disc text-xs w-full sm:w-[650px]">
                 <li>Authentication — Instagram Basic Display is not an authentication solution. Data returned by the API cannot be used to authenticate your app users or log them into your app. If you need an authentication solution we recommend using Facebook Login instead.</li>
                 <li>App Types — Apps designated as Business apps are not supported. If your app is a Business app use the Instagram Graph API instead or create a new non-Business app.</li>
                 <li>Legacy IDs — Instagram Legacy API User IDs are not supported.</li>
@@ -52,7 +52,7 @@ const Blog = () => {
                 <li>Get an Instagram user’s images, videos, and albums</li>
             </ul>
             <p className="text-white text-xl mt-8">Limitations</p>
-            <ul className="text-white list-disc text-xs w-[650px]">
+            <ul className="text-white list-disc text-xs w-full sm:w-[650px]">
                 <li>Authentication — Instagram Basic Display is not an authentication solution. Data returned by the API cannot be used to authenticate your app users or log them into your app. If you need an authentication solution we recommend using Facebook Login instead.</li>
                 <li>App Types — Apps designated as Business apps are not supported. If your app is a Business app use the Instagram Graph API instead or create a new non-Business app.</li>
                 <li>Legacy IDs — Instagram Legacy API User IDs are not supported.</li>
