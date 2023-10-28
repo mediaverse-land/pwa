f7f02641e12998b8a2f9fec55d384c30dbae6dbe
Smok455 <46872648+Smok455@users.noreply.github.com>
2023-10-28 14:46:17 +0330

    fetch instance

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -2,39 +2,49 @@ import http from "./http";
 
 export const URL = "https://api.mediaverse.land/v2"
 
+const fetchInstance = (url: string) => {
+    return fetch(`${URL}/images/most-viewed`, {
+        cache: 'no-store',
+        headers: {
+            'Accept-Language': 'en-US',
+            'x-app': '_Web'
+        },
+    })
+}
+
 export const getMostViewedImages = async () => {
     const url = `${URL}/images/most-viewed`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 };
 export const getMostViewedText = async () => {
     const url = `${URL}/texts/most-viewed`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 };
 
 export const getMostViewedVideos = () => {
     const url = `${URL}/videos/most-viewed`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 };
 
 export const getMostViewedSongs = () => {
     const url = `${URL}/audios/most-viewed`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 };
 
 export const getTerms = () => {
     const url = `${URL}/terms`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 }
 
 export const getPrivacy = () => {
     const url = `${URL}/privacy`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 }
 export const getLives = async () => {
     const url = `${URL}/lives`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 };
 export const getFAQ = () => {
     const url = `${URL}/faq`;
-    return fetch(url, { cache: 'no-store' });
+    return fetchInstance(url);
 }
