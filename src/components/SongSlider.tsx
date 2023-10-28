cc6d31b8f2ecd72fe974db7055005a63a1a54e6b
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-10-28 11:09:06 +0330

    add ScrollToBottom function and edit sliders styles

--- src/components/SongSlider.tsx
+++ src/components/SongSlider.tsx
@@ -1,58 +1,92 @@
-'use client'
-import { Swiper, SwiperSlide, } from "swiper/react";
-import 'swiper/css';
-import { Swiper as SwiperClass } from 'swiper/types';
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Swiper as SwiperClass } from "swiper/types";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 import { getMostViewedSongs } from "@/services/contactService";
 const SongSlider = () => {
+  const [swiperRef, setSwiperRef] = useState<SwiperClass>();
+  const [songoData, setSongData] = useState([]);
 
-    const [swiperRef, setSwiperRef] = useState<SwiperClass>();
-    const [songoData, setSongData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const songData = await getMostViewedSongs();
+      const data = await songData.json();
+      setSongData(data);
+    };
+    getData();
+  }, []);
 
-    useEffect(() => {
-        const getData = async () => {
-            const songData = await getMostViewedSongs();
-            const data = await songData.json()
-            setSongData(data);
-        };
-        getData();
-    }, []);
+  const handlePrevious = useCallback(() => {
+    swiperRef?.slidePrev();
+  }, [swiperRef]);
 
-    const handlePrevious = useCallback(() => {
-        swiperRef?.slidePrev();
-    }, [swiperRef]);
+  const handleNext = useCallback(() => {
+    swiperRef?.slideNext();
+  }, [swiperRef]);
 
-    const handleNext = useCallback(() => {
-        swiperRef?.slideNext();
-    }, [swiperRef]);
-
-    return (<div className=" flex space-x-0 sm:space-x-4 justify-center items-center mt-10 ">
-        <button className=" hidden sm:block " onClick={handleNext}>
-            <Image src="/icons/prev.png" alt="arrow" width={24} height={24} quality={100} />
-        </button>
+  return (
+    <div className=" flex space-x-0 sm:space-x-4 justify-center items-center mt-10 ">
+      <button className=" hidden sm:block " onClick={handleNext}>
+        <Image
+          src="/icons/prev.png"
+          alt="arrow"
+          width={24}
+          height={24}
+          quality={100}
+        />
+      </button>
+      <div className="relative w-[80rem] mx-auto max-w-screen-2xl">
         <Swiper
-            slidesPerView={8.5}
-            spaceBetween={2}
-            onSwiper={setSwiperRef}
-
+          centerInsufficientSlides
+          className="w-full linear-black-bg rounded-2xl"
+          slidesPerView={7.5}
+          //   spaceBetween={2}
+          onSwiper={setSwiperRef}
+          loop={true}
+          rewind={true}
         >
-            {songoData.map((item: any, index) => {
-                return <SwiperSlide>
-                    <img src={item.asset.thumbnails["226x226"]} className="rounded-xl" alt="Music Cover" height={120} width={140} />
-                    <p className="mt-2 ml-1 text-gray-600 text-sm">{item.name}</p>
-                    <div className="flex ml-1 mt-1 space-x-2">
-                        <Image src="/images/mini-avatar.png" alt="avatar" width={16} height={16} quality={100} />
-                        <p className="text-xs text-gray-500">{item.asset.user.username}</p>
-                    </div>
-                </SwiperSlide>
-            })}
+          {songoData.map((item: any, index) => {
+            return (
+              <SwiperSlide className="w-[140px] h-[120px]" key={item.id}>
+                <div className="relative w-[140px] h-[120px]">
+                  <Image
+                    src={item.asset.thumbnails["226x226"]}
+                    className="rounded-xl"
+                    alt="Music Cover"
+                    fill
+                  />
+                </div>
+                <p className="mt-2 ml-1 text-gray-600 text-sm">{item.name}</p>
+                <div className="flex ml-1 mt-1 space-x-2">
+                  <Image
+                    src="/images/mini-avatar.png"
+                    alt="avatar"
+                    width={16}
+                    height={16}
+                    quality={100}
+                  />
+                  <p className="text-xs text-gray-500">
+                    {item.asset.user.username}
+                  </p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
-        <button className="hidden sm:block" onClick={handlePrevious}>
-            <Image src="/icons/next.png" alt="arrow" width={24} height={24} quality={100} />
-        </button>
-
-    </div>);
-}
+      </div>
+      <button className="hidden sm:block" onClick={handlePrevious}>
+        <Image
+          src="/icons/next.png"
+          alt="arrow"
+          width={24}
+          height={24}
+          quality={100}
+        />
+      </button>
+    </div>
+  );
+};
 
 export default SongSlider;
--- src/components/VideoSlider.tsx
+++ src/components/VideoSlider.tsx
@@ -1,58 +1,103 @@
-"use client"
-import { Swiper, SwiperSlide, } from "swiper/react";
-import 'swiper/css';
-import { Swiper as SwiperClass } from 'swiper/types';
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Swiper as SwiperClass } from "swiper/types";
 import Image from "next/image";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { getMostViewedVideos } from "@/services/contactService";
+import { Autoplay, FreeMode } from "swiper/modules";
 
 const VideoSlider = () => {
-    const [swiperRef, setSwiperRef] = useState<SwiperClass>();
-    const [videoData, setVideoData] = useState([]);
+  const [swiperRef, setSwiperRef] = useState<SwiperClass>();
+  const [videoData, setVideoData] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-          const videoData = await getMostViewedVideos();
-          const data = await videoData.json()
-          setVideoData(data);
-        };
-        getData();
-      }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const videoData = await getMostViewedVideos();
+      const data = await videoData.json();
+      setVideoData(data);
+    };
+    getData();
+  }, []);
+  console.log(videoData, "videodata");
+  const handlePrevious = useCallback(() => {
+    swiperRef?.slidePrev();
+  }, [swiperRef]);
 
-    const handlePrevious = useCallback(() => {
-        swiperRef?.slidePrev();
-    }, [swiperRef]);
-
-    const handleNext = useCallback(() => {
-        swiperRef?.slideNext();
-    }, [swiperRef]);
-    return (<div className=" flex space-x-0 sm:space-x-4 justify-center items-center mt-10 ">
-        <button className=" hidden sm:block " onClick={handleNext}>
-            <Image src="/icons/prev.png" alt="arrow" width={24} height={24} quality={100} />
-        </button>
+  const handleNext = useCallback(() => {
+    swiperRef?.slideNext();
+  }, [swiperRef]);
+  return (
+    <div className=" flex space-x-0 sm:space-x-4 justify-center items-center mt-10 ">
+      <button className=" hidden sm:block " onClick={handleNext}>
+        <Image
+          src="/icons/prev.png"
+          alt="arrow"
+          width={24}
+          height={24}
+          quality={100}
+        />
+      </button>
+      <div className="relative w-[80rem] max-w-screen-2xl mx-auto">
+        {/* shadow right */}
+        {/* <div className="absolute w-[9rem] h-full top-0 right-0 z-20 linear-black-bg"></div> */}
+        {/* shadow left */}
+        <div></div>
         <Swiper
-            slidesPerView={8.5}
-            spaceBetween={2}
-            onSwiper={setSwiperRef}
-            loop={true            }
-           
+          modules={[Autoplay, FreeMode]}
+          centerInsufficientSlides
+          slidesPerView={6.5}
+          // freeMode={true}
+          // spaceBetween={16}
+          onSwiper={setSwiperRef}
+          loop={true}
+          rewind={true}
+          // autoplay={{ delay: 5 }}
+          className="w-full linear-black-bg rounded-2xl"
         >
-            {videoData.map((item : any,index) => {
-                return <SwiperSlide>
-                <img src={item.asset.thumbnails["226x226"]} className="rounded-xl" alt="video cover" height={120} width={140} />
+          {videoData.map((item: any, index) => {
+            return (
+              <SwiperSlide
+                key={item.id}
+                className="w-[150px] h-[190px] cursor-pointer rounded-2xl"
+              >
+                <div className="relative w-[194px] aspect-square">
+                  <Image
+                    src={item.asset.thumbnails["226x226"]}
+                    className="rounded-xl"
+                    alt="video cover"
+                    fill
+                  />
+                </div>
                 <p className="mt-2 ml-1 text-gray-600 text-sm">{item.name}</p>
                 <div className="flex ml-1 mt-1 space-x-2">
-                    <Image src="/images/mini-avatar.png" alt="avatar" width={16} height={16} quality={100} />
-                    <p className="text-xs text-gray-500">{item.asset.user.username}</p>
+                  <Image
+                    src="/images/mini-avatar.png"
+                    alt="avatar"
+                    width={16}
+                    height={16}
+                    quality={100}
+                  />
+                  <p className="text-xs text-gray-500">
+                    {item.asset.user.username}
+                  </p>
                 </div>
-            </SwiperSlide>
-            })}
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
-        <button className="hidden sm:block" onClick={handlePrevious}>
-            <Image src="/icons/next.png" alt="arrow" width={24} height={24} quality={100} />
-        </button>
-
-    </div>);
-}
+      </div>
+      <button className="hidden sm:block" onClick={handlePrevious}>
+        <Image
+          src="/icons/next.png"
+          alt="arrow"
+          width={24}
+          height={24}
+          quality={100}
+        />
+      </button>
+    </div>
+  );
+};
 
 export default VideoSlider;
