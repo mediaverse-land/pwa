e6f2aca4d491ed23eb2f450878299062e3e8e69b
Smok455 <46872648+Smok455@users.noreply.github.com>
2023-10-30 15:43:33 +0330

    change api address

--- src/app/APIs/page.tsx
+++ src/app/APIs/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function api() {
   return (
     <div className="h-[calc(100vh-195px)] overflow-auto mt-14">
-      <iframe src="https://docs.mediaverse.land" className="w-full h-full" />
+      <iframe src="https://swagger.mediaverse.land" className="w-full h-full" />
     </div>
   );
 }
