c5bbb9377a8f0efc579e138a3b42fa0da7fb2dbf
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-10-31 15:34:32 +0330

    add authentication template

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import Motion from "@/components/motion";
+
+const Login = () => {
+  return (
+    <Motion>
+      <div className="mt-28">sing in</div>
+    </Motion>
+  );
+};
+
+export default Login;
