989584a9159fed843526aaa7a2389c761bf51d19
Smok455 <46872648+Smok455@users.noreply.github.com>
2023-11-05 14:31:51 +0330

    first page grid system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -173,18 +173,18 @@ const Home = async () => {
               />
               <p className="text-white text-sm ">Most viewed</p>
             </div>
-            <div className="grid grid-rows-4 grid-flow-col gap-2 mt-10 grow max-h-[510px]">
+            <div className="grid grid-rows-4 grid-cols-3 grid-flow-row gap-2 mt-10 grow max-h-[510px]">
               {imageData.map((items: any, index: number) => {
+
                 return (
                   <ClientImage
                     key={index}
-                    className={`${
-                      index === 4
-                        ? "rounded-xl w-full aspect-square col-span-2 row-span-2"
-                        : "rounded-xl w-full aspect-square"
+                    className={`${index  === 1
+                      ? "rounded-xl w-full aspect-square col-span-2 row-span-2"
+                      : "rounded-xl w-full aspect-square col-span-1 row-span-1"
                     }`}
                     src={items.asset.thumbnails["336x366"]}
-                    alt="photo"
+                    alt={items.name}
                   />
                 );
               })}
