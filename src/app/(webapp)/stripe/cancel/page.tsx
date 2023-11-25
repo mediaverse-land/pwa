d74ae5be7b6b667d54570f8c8b43b7162edb6139
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-11-25 22:09:58 +0330

    typo

--- src/app/(webapp)/stripe/cancel/page.tsx
+++ src/app/(webapp)/stripe/cancel/page.tsx
@@ -2,7 +2,7 @@ const StripeCancel = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <p className="border rounded-2xl py-14 px-8 text-[14px] max-w-[286px] lg:px-20 lg:py-20 text-white lg:text-[18px] font-semibold bg-[rgba(78,78,97,0.50)] md:max-w-[568px] md:text-[16px] lg:max-w-[768px] mx-auto text-center">
-        We're sorry, but it seems your session has ended.
+        We're sorry, but it seems your session has canceled.
       </p>
     </div>
   );
