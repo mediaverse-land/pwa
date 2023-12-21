583328a541e949087ee2ea0f4a3749f874e1e022
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-12-21 23:02:55 +0330

    test sitemap

--- src/app/terms/sitemap.ts
+++ src/app/terms/sitemap.ts
@@ -1,10 +1,10 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // console.log("Test Sitemap");
+  console.log("Test Sitemap");
   return [
     {
-      url: "https://mediaverse.land",
+      url: "https://mediaverse.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
