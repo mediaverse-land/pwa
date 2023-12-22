e14b45c8931023751f0d8cb3fd7bac1e5e1ca0db
Smok455 <46872648+Smok455@users.noreply.github.com>
2023-12-22 11:09:47 +0330

    sitemaps

---
+++ src/app/(webapp)/(main)/app/assets/sitemap.ts
@@ -0,0 +1,42 @@
+import { getSitemap } from "@/services/contactService";
+import { MetadataRoute } from "next";
+
+export default async function assets_sitemap(): Promise<MetadataRoute.Sitemap> {
+    const d = await getSitemap()
+
+    const data = await d.json()
+
+    return [
+        {
+            url: process.env.NEXTAUTH_URL + "/app/explore",
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 1,
+        },
+        ...data.images.map((x: any, i: any) => ({
+            url: `${process.env.NEXTAUTH_URL}/app/assets/image/${x.name}?id=${x.id}`,
+            lastModified: x.updated_at,
+            changeFrequency: "weekly",
+            priority: 1,
+        })),
+        ...data.videos.map((x: any, i: any) => ({
+            url: `${process.env.NEXTAUTH_URL}/app/assets/video/${x.name}?id=${x.id}`,
+            lastModified: x.updated_at,
+            changeFrequency: "weekly",
+            priority: 1,
+        })),
+        ...data.audio.map((x: any, i: any) => ({
+            url: `${process.env.NEXTAUTH_URL}/app/assets/audio/${x.name}?id=${x.id}`,
+            lastModified: x.updated_at,
+            changeFrequency: "weekly",
+            priority: 1,
+        })),
+        ...data.text.map((x: any, i: any) => ({
+            url: `${process.env.NEXTAUTH_URL}/app/assets/text/${x.name}?id=${x.id}`,
+            lastModified: x.updated_at,
+            changeFrequency: "weekly",
+            priority: 1,
+        }))
+
+    ];
+}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,53 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // console.log("Test Sitemap");
   return [
     {
-      url: "https://mediaverse.land",
+      url: process.env.NEXTAUTH_URL + "",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/blogs",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/terms",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/privacy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/faq",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/about-us",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/app/assets/sitemap.xml",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: process.env.NEXTAUTH_URL + "/blogs/sitemap.xml",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 1,
     },
   ];
