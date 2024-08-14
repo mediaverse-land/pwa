b5aad315cfe72b952da7afcf42525bd9fed7f232
Mohammad Alimardani <alimardani94@gmail.com>
2024-08-14 19:14:44 +0330

    update

--- src/configs/base.ts
+++ src/configs/base.ts
@@ -1,15 +1,16 @@
 import { Locale } from "@/types/dictionary-types";
 
 export const baseURL = process.env.BASE_URL;
-export const blogURL = process.env.BLOG_URL;
-export const swaggerURL = process.env.SWAGGER_URL;
-export const websiteTitle = process.env.APP_TITLE;
-// logo url relative to public folder
-export const logoURL = "/images/logo.png";
-export const webAppDeepLink = process.env.DEEP_LINK;
-export const playStoreLink = process.env.PLAY_STORE_LINK;
 
-export const activeLocales = ["de", "en", "fr"] as Locale[];
+export const websiteTitle = process.env.NEXT_PUBLIC_APP_TITLE;
+export const blogURL = process.env.NEXT_PUBLIC_BLOG_URL;
+export const swaggerURL = process.env.NEXT_PUBLIC_SWAGGER_URL;
+export const webAppDeepLink = process.env.NEXT_PUBLIC_DEEP_LINK;
+export const playStoreLink = process.env.NEXT_PUBLIC_PLAY_STORE_LINK;
+
+export const activeLocales = (process.env.NEXT_PUBLIC_ACTIVE_LOCALES?.split(',') || ["en"]) as Locale[];
+
+export const logoURL = "/images/logo.png";
 
 export const imagePlaceHolders = {
   image: "/images/No-Image.png",
